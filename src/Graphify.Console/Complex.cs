cf8893ad581838a64493737e84eab96882ec8c56
Paul Martins <vmartins.paul@gmail.com>
2026-01-16 09:23:41 +0000

    Set test language version to 3

---
+++ src/Graphify.Console/Complex.cs
@@ -0,0 +1,20 @@
+﻿namespace Graphify.Console;
+
+using System.Collections.Immutable;
+
+[Graphify]
+internal sealed partial class Complex
+{
+    public string Age { get; set; } = string.Empty;
+
+    public ImmutableArray<Child> Children { get; set; } = ImmutableArray<Child>.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public sealed partial class Child
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+    }
+}

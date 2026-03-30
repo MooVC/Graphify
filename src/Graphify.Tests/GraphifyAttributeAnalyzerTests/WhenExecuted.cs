a78b40d87cc1af8722ec400002508b682263f8d5
Paul Martins <50200071+MooVC@users.noreply.github.com>
2026-03-30 20:06:50 +0100

    Sort analyzer test methods alphabetically

--- src/Graphify.Tests/GraphifyAttributeAnalyzerTests/WhenExecuted.cs
+++ src/Graphify.Tests/GraphifyAttributeAnalyzerTests/WhenExecuted.cs
@@ -11,13 +11,13 @@ using AnalyzerTest = Graphify.AnalyzerTest<Graphify.GraphifyAttributeAnalyzer>;
 public sealed class WhenExecuted
 {
     [Theory]
-    [Snippets(inclusions: [typeof(Unsupported)])]
-    public async Task GivenATypeWhenNotPartialThenPartialTypeRuleIsRaised(ReferenceAssemblies assembly, Expectations expectations, LanguageVersion language)
+    [Snippets(inclusions: [typeof(Generic)])]
+    public async Task GivenATypeWhenGenericThenGenericTypeRuleIsRaised(ReferenceAssemblies assembly, Expectations expectations, LanguageVersion language)
     {
         // Arrange
         var test = new AnalyzerTest(assembly, language);
 
-        test.ExpectedDiagnostics.Add(GetExpectedPartialTypeRule(new LinePosition(2, 1), nameof(Unsupported)));
+        test.ExpectedDiagnostics.Add(GetExpectedGenericTypeRule(new LinePosition(2, 1), nameof(Generic)));
         expectations.IsDeclaredIn(test.TestState);
 
         // Act
@@ -28,13 +28,13 @@ public sealed class WhenExecuted
     }
 
     [Theory]
-    [Snippets(inclusions: [typeof(Generic)])]
-    public async Task GivenATypeWhenGenericThenGenericTypeRuleIsRaised(ReferenceAssemblies assembly, Expectations expectations, LanguageVersion language)
+    [Snippets(inclusions: [typeof(Unsupported)])]
+    public async Task GivenATypeWhenNotPartialThenPartialTypeRuleIsRaised(ReferenceAssemblies assembly, Expectations expectations, LanguageVersion language)
     {
         // Arrange
         var test = new AnalyzerTest(assembly, language);
 
-        test.ExpectedDiagnostics.Add(GetExpectedGenericTypeRule(new LinePosition(2, 1), nameof(Generic)));
+        test.ExpectedDiagnostics.Add(GetExpectedPartialTypeRule(new LinePosition(2, 1), nameof(Unsupported)));
         expectations.IsDeclaredIn(test.TestState);
 
         // Act
